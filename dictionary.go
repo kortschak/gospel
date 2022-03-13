e3a69d7a10d8a4ae8d89341eea1c043f1f3ace35
Dan Kortschak <dan@kortschak.io>
2022-03-14 09:06:34 +1030

    fix doc comment

--- dictionary.go
+++ dictionary.go
@@ -1,6 +1,7 @@
 // Copyright ©2022 Dan Kortschak. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
 package main
 
 import (
